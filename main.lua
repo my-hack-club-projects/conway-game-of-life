bc35929a447f5d333779a2f04408cf6a6d291a9e
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-08 17:00:12 +0200

    update the cells in the grid depending on the amount of neighbors

--- main.lua
+++ main.lua
@@ -5,12 +5,59 @@ local grid = {}
 local updateTime = 0.1
 local timer = 0
 
+math.randomseed(os.time()) -- Seed depending on the current time
+
+function countNeighbors(x, y)
+    local count = 0
+    for j = -1, 1 do
+        for i = -1, 1 do
+            if not (i == 0 and j == 0) then
+                local nx, ny = x + i, y + j
+                if nx > 0 and ny > 0 and nx <= gridWidth and ny <= gridHeight then
+                    count = count + grid[ny][nx]
+                end
+            end
+        end
+    end
+    return count
+end
+
+function updateGrid()
+    local newGrid = {}
+    for y = 1, gridHeight do
+        newGrid[y] = {}
+        for x = 1, gridWidth do
+            local alive = grid[y][x] == 1
+            local neighbors = countNeighbors(x, y)
+
+            if alive and (neighbors < 2 or neighbors > 3) then
+                newGrid[y][x] = 0  -- Cell dies
+            elseif alive and (neighbors == 2 or 3) then
+                newGrid[y][x] = 1  -- Cell lives
+            elseif not alive and neighbors == 3 then
+                newGrid[y][x] = 1  -- Cell becomes alive
+            else
+                newGrid[y][x] = grid[y][x]
+            end
+        end
+    end
+    grid = newGrid
+end
+
 function love.load()
     for y = 1, gridHeight do
         grid[y] = {}
         for x = 1, gridWidth do
             grid[y][x] = math.random(2) - 1  -- Randomly initialize cells
         end
+    end
+end
+
+function love.update(dt)
+    timer = timer + dt
+    if timer >= updateTime then
+        updateGrid()
+        timer = 0
     end
 end
 
